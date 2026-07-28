1d4a92bb8bddf942400b0eaff6ad43db1a0a28e2
CamilaN07 <mariacamilaninosalamanca@gmail.com>
2026-07-28 08:36:31 -0500

    endpoint de producto categoria

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-//import express from "express";//ES
-const express = require("express")
+import express from "express";//ES
+//import {configDotenv} from "dotenv"
+//configDotenv()
+import "dotenv/config"
+//const express = require("express");
 const app = express();
-const puerto = procses.env.PORT || 3000;
+const puerto = process.env.PORT || 3500;
 
 app.get("/",(req,res)=> {
-    res.send("Hola ficha 3407180");
+    res.send("Hola ficha 3407180 del SENA");
+});
+
+app.get("/MiSaludo",(req,res)=> {
+    res.send(`<h1>Hola soy Camila de la ficha 3407180<h1/>
+        <p>del SENA</p>`);
+});
+
+app.get("/MiSaludo/:id",(req,res)=> {
+    const id= req.params.id;
+    res.send(`<h1>Saludo<h1/>
+        <p>Hola soy Camila de la ficha con ID ${id}</p>`);
+});
+
+//Productos-categoria
+app.get("/productos/:categoria",(req,res)=> {
+    const categoria= req.params.categoria;
+    res.send(`<h1>Productos</h1>
+         <p>Categoría: ${categoria}</p>`);
 });
 
 app.listen(puerto, ()=> {
-    console.log(`Servidor funsionando!! en el puerto ${puerto}`);
+    console.log(`Servidor http://localhost:${puerto}
+        http://127.0.0.1:${puerto}`);
 });
+
